d3e7ea3a9ef8f757ac0f224492dfd112eb021bb6
Trần Ngọc Đức <tranngocduc24121999h@gmail.com>
2022-03-07 17:29:55 +0700

    baithuchanhjava

--- BaiThucHanh0703/App74.java
+++ BaiThucHanh0703/App74.java
@@ -19,3 +19,26 @@ public static void main(String[] args) {​
   for (int i = 0; i < n; i++) {​
 
   System.out.print("Nhập phần tử thứ "+i + ": ");​
+  A[i] = scanner.nextInt();​
+
+    }​
+
+   // Tính tổng các số chẵn có trong mảng​
+
+    double Tong =0;​
+
+    for (int i = 0; i < n; i++)// {​
+
+        if (A[i] % 2 == 0) {​
+
+            Tong+=A[i];​
+
+        }​
+
+    }​
+
+    System.out.println("Tỏng: " + Tong);​
+
+   }​
+
+​
